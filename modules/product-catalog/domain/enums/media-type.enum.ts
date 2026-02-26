f50bd02a97155741bea1bd07e331e5729ef55834
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-02-26 10:38:39 +0530

    add MediaType and MediaProvider enums for media handling

---
+++ modules/product-catalog/domain/enums/media-type.enum.ts
@@ -0,0 +1,11 @@
+export enum MediaType {
+  IMAGE = "image",
+  VIDEO = "video",
+  THREE_D_MODEL = "3d_model",
+}
+
+export enum MediaProvider {
+  LOCAL = "local",
+  CLOUDINARY = "cloudinary",
+  S3 = "s3",
+}
