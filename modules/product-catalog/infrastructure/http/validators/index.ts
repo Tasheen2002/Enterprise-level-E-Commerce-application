6253a9f98a0f76fdf1b58290aee32115f20c30e9
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-02-26 10:50:43 +0530

    add validators for product, category, variant, media, and search

---
+++ modules/product-catalog/infrastructure/http/validators/index.ts
@@ -0,0 +1,5 @@
+export * from "./product.validator";
+export * from "./category.validator";
+export * from "./variant.validator";
+export * from "./media.validator";
+export * from "./search.validator";
