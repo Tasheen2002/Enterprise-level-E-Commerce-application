1db71690bdfe632ff207aac618a50b9157bdbf55
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-02-26 17:21:59 +0530

    created index file for location queries

---
+++ modules/inventory-management/application/queries/index.ts
@@ -0,0 +1,7 @@
+export * from "./stock";
+export * from "./location";
+export * from "./supplier";
+export * from "./purchase-order";
+export * from "./alert";
+export * from "./pickup-reservation";
+export * from "./transaction";
