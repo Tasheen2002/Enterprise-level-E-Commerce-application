107ca1e367da25b874aadfeb54ae44830f3fd26d
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-02-26 17:20:32 +0530

    created index file for stock queries

---
+++ modules/inventory-management/application/queries/stock/index.ts
@@ -0,0 +1,6 @@
+export * from "./get-stock.query";
+export * from "./get-stock-stats.query";
+export * from "./get-stock-by-variant.query";
+export * from "./list-stocks.query";
+export * from "./get-low-stock-items.query";
+export * from "./get-total-available-stock.query";
