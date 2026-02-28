8f1a90b7c50a7018cfba83d96e818bcef90d0ea4
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-02-28 15:14:04 +0530

    remove unused type exports from user management services

--- modules/product-catalog/application/services/index.ts
+++ modules/product-catalog/application/services/index.ts
@@ -1,12 +1,9 @@
-// Export all services
 export { ProductManagementService } from "./product-management.service";
 export { CategoryManagementService } from "./category-management.service";
 export { MediaManagementService } from "./media-management.service";
 export { VariantManagementService } from "./variant-management.service";
 export { ProductSearchService } from "./product-search.service";
 export { SlugGeneratorService } from "./slug-generator.service";
-
-// Additional management services
 export { ProductMediaManagementService } from "./product-media-management.service";
 export { VariantMediaManagementService } from "./variant-media-management.service";
 export { ProductTagManagementService } from "./product-tag-management.service";
--- modules/user-management/application/services/index.ts
+++ modules/user-management/application/services/index.ts
@@ -4,35 +4,3 @@ export { AddressManagementService } from "./address-management.service";
 export { PaymentMethodService } from "./payment-method.service";
 export { UserProfileService } from "./user-profile.service";
 export { VerificationService } from "./verification.service";
-
-export type { IPasswordHasherService } from "./password-hasher.service";
-export type {
-  LoginCredentials,
-  AuthResult,
-  LoginResult,
-  TokenPayload,
-  RefreshTokenResult,
-  RegisterUserData,
-} from "./authentication.service";
-export type {
-  AddAddressDto,
-  UpdateAddressDto,
-  AddressResponseDto,
-  AddressStatsDto,
-} from "./address-management.service";
-export type {
-  AddPaymentMethodDto,
-  UpdatePaymentMethodDto,
-  PaymentMethodResponseDto,
-  PaymentMethodValidationResult,
-} from "./payment-method.service";
-export type {
-  UserProfileDto,
-  UpdateUserProfileDto,
-  UserProfileWithDetails,
-} from "./user-profile.service";
-export type {
-  EmailService,
-  VerificationResult,
-  VerificationContext,
-} from "./verification.service";
