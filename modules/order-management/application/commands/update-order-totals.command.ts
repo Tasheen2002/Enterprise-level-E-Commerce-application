a7663e819e8f0ff77d72e5a6b2306e0bc4a141a3
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-03-05 14:05:10 +0530

    Refactor UpdateOrderTotalsCommand to remove subtotal and total fields

--- modules/order-management/application/commands/update-order-totals.command.ts
+++ modules/order-management/application/commands/update-order-totals.command.ts
@@ -3,10 +3,8 @@ import { ICommand } from "@/api/src/shared/application";
 export interface UpdateOrderTotalsCommand extends ICommand {
   orderId: string;
   totals: {
-    subtotal: number;
-    discount: number;
     tax: number;
     shipping: number;
-    total: number;
+    discount: number;
   };
 }
