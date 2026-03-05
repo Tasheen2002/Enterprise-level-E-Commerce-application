cb66d8e57959aed08c4993fd82b3d42446946c44
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-03-05 14:36:53 +0530

    Add user management domain exports for entities, value objects, repositories, errors, and constants

---
+++ modules/user-management/domain/index.ts
@@ -0,0 +1,17 @@
+export * from "./entities";
+export { UserId } from "./value-objects/user-id.vo";
+export { Email } from "./value-objects/email.vo";
+export { Phone } from "./value-objects/phone.vo";
+export { Password } from "./value-objects/password.vo";
+export { Currency } from "./value-objects/currency.vo";
+export { Locale } from "./value-objects/locale.vo";
+export { Address as AddressVO, AddressType } from "./value-objects/address.vo";
+export type {
+  AddressData as AddressVOData,
+  FormattedAddress,
+} from "./value-objects/address.vo";
+export type { PasswordPolicy } from "./value-objects/password.vo";
+export { DEFAULT_PASSWORD_POLICY } from "./value-objects/password.vo";
+export * from "./repositories";
+export * from "./errors";
+export * from "./constants";
