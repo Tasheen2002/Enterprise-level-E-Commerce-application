94f159c56bd89529adba8db3269b9ec3668a6418
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-03-05 14:30:03 +0530

    Refactor command exports for better readability and add Logout, RefreshToken, and DeleteAccount commands

--- modules/user-management/application/commands/index.ts
+++ modules/user-management/application/commands/index.ts
@@ -4,50 +4,119 @@ export { RegisterUserHandler } from "./register-user.handler";
 export type { LoginUserCommand } from "./login-user.command";
 export { LoginUserHandler } from "./login-user.handler";
 
-export type { UpdateProfileCommand, UpdateProfileResult } from "./update-profile.command";
+export type {
+  UpdateProfileCommand,
+  UpdateProfileResult,
+} from "./update-profile.command";
 export { UpdateProfileHandler } from "./update-profile.handler";
 
-export type { AddAddressCommand, AddAddressResult } from "./add-address.command";
+export type {
+  AddAddressCommand,
+  AddAddressResult,
+} from "./add-address.command";
 export { AddAddressHandler } from "./add-address.handler";
 
-export type { UpdateAddressCommand, UpdateAddressResult } from "./update-address.command";
+export type {
+  UpdateAddressCommand,
+  UpdateAddressResult,
+} from "./update-address.command";
 export { UpdateAddressHandler } from "./update-address.handler";
 
-export type { DeleteAddressCommand, DeleteAddressResult } from "./delete-address.command";
+export type {
+  DeleteAddressCommand,
+  DeleteAddressResult,
+} from "./delete-address.command";
 export { DeleteAddressHandler } from "./delete-address.handler";
 
-export type { AddPaymentMethodCommand, AddPaymentMethodResult } from "./add-payment-method.command";
+export type {
+  AddPaymentMethodCommand,
+  AddPaymentMethodResult,
+} from "./add-payment-method.command";
 export { AddPaymentMethodHandler } from "./add-payment-method.handler";
 
-export type { UpdatePaymentMethodCommand, UpdatePaymentMethodResult } from "./update-payment-method.command";
+export type {
+  UpdatePaymentMethodCommand,
+  UpdatePaymentMethodResult,
+} from "./update-payment-method.command";
 export { UpdatePaymentMethodHandler } from "./update-payment-method.handler";
 
-export type { DeletePaymentMethodCommand, DeletePaymentMethodResult } from "./delete-payment-method.command";
+export type {
+  DeletePaymentMethodCommand,
+  DeletePaymentMethodResult,
+} from "./delete-payment-method.command";
 export { DeletePaymentMethodHandler } from "./delete-payment-method.handler";
 
-export type { ChangePasswordCommand, ChangePasswordResult } from "./change-password.command";
+export type {
+  SetDefaultPaymentMethodCommand,
+  SetDefaultPaymentMethodResult,
+} from "./set-default-payment-method.command";
+export { SetDefaultPaymentMethodHandler } from "./set-default-payment-method.handler";
+
+export type {
+  ChangePasswordCommand,
+  ChangePasswordResult,
+} from "./change-password.command";
 export { ChangePasswordHandler } from "./change-password.handler";
 
-export type { ChangeEmailCommand, ChangeEmailResult } from "./change-email.command";
+export type {
+  ChangeEmailCommand,
+  ChangeEmailResult,
+} from "./change-email.command";
 export { ChangeEmailHandler } from "./change-email.handler";
 
-export type { InitiatePasswordResetCommand, InitiatePasswordResetResult } from "./initiate-password-reset.command";
+export type {
+  InitiatePasswordResetCommand,
+  InitiatePasswordResetResult,
+} from "./initiate-password-reset.command";
 export { InitiatePasswordResetHandler } from "./initiate-password-reset.handler";
 
-export type { ResetPasswordCommand, ResetPasswordResult } from "./reset-password.command";
+export type {
+  ResetPasswordCommand,
+  ResetPasswordResult,
+} from "./reset-password.command";
 export { ResetPasswordHandler } from "./reset-password.handler";
 
-export type { VerifyEmailCommand, VerifyEmailResult } from "./verify-email.command";
+export type {
+  VerifyEmailCommand,
+  VerifyEmailResult,
+} from "./verify-email.command";
 export { VerifyEmailHandler } from "./verify-email.handler";
 
-export type { UpdateUserRoleCommand, UpdateUserRoleResult } from "./update-user-role.command";
+export type {
+  UpdateUserRoleCommand,
+  UpdateUserRoleResult,
+} from "./update-user-role.command";
 export { UpdateUserRoleHandler } from "./update-user-role.handler";
 
-export type { UpdateUserStatusCommand, UpdateUserStatusResult } from "./update-user-status.command";
+export type {
+  UpdateUserStatusCommand,
+  UpdateUserStatusResult,
+} from "./update-user-status.command";
 export { UpdateUserStatusHandler } from "./update-user-status.handler";
 
-export type { DeleteUserCommand, DeleteUserResult } from "./delete-user.command";
+export type {
+  DeleteUserCommand,
+  DeleteUserResult,
+} from "./delete-user.command";
 export { DeleteUserHandler } from "./delete-user.handler";
 
-export type { ToggleUserEmailVerifiedCommand, ToggleUserEmailVerifiedResult } from "./toggle-user-email-verified.command";
+export type {
+  ToggleUserEmailVerifiedCommand,
+  ToggleUserEmailVerifiedResult,
+} from "./toggle-user-email-verified.command";
 export { ToggleUserEmailVerifiedHandler } from "./toggle-user-email-verified.handler";
+
+export type { LogoutCommand, LogoutResult } from "./logout.command";
+export { LogoutHandler } from "./logout.handler";
+
+export type {
+  RefreshTokenCommand,
+  RefreshTokenCommandResult,
+} from "./refresh-token.command";
+export { RefreshTokenHandler } from "./refresh-token.handler";
+
+export type {
+  DeleteAccountCommand,
+  DeleteAccountResult,
+} from "./delete-account.command";
+export { DeleteAccountHandler } from "./delete-account.handler";
