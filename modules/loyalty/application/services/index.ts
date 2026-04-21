c07f90bbd206e6d988981d617815dc455e2e73ea
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-04-22 02:31:35 +0530

    Refactor loyalty service exports to improve type definitions and enhance clarity

--- modules/loyalty/application/services/index.ts
+++ modules/loyalty/application/services/index.ts
@@ -1,5 +1,5 @@
 export { LoyaltyService } from './loyalty.service';
-export type { EarnPointsData, RedeemPointsData, AdjustPointsData, LoyaltyAccountData } from './loyalty.service';
-export { LoyaltyTransactionService } from './loyalty-transaction.service';
-export { LoyaltyAccountService } from './loyalty-account.service';
+export type { EarnPointsData, RedeemPointsData, AdjustPointsData, LoyaltyAccountDetailsDTO } from './loyalty.service';
+
 export { LoyaltyProgramService } from './loyalty-program.service';
+export type { CreateLoyaltyProgramData } from './loyalty-program.service';
