46de6342b4bfdb42185dfdb07da5a4e6125260fa
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-05-06 17:26:54 +0530

    Refactor repository interfaces to streamline methods and enhance clarity in cart, checkout, and reservation modules

--- modules/cart/domain/repositories/checkout.repository.ts
+++ modules/cart/domain/repositories/checkout.repository.ts
@@ -4,6 +4,7 @@ import { CartId } from "../value-objects/cart-id.vo";
 import { CartOwnerId } from "../value-objects/cart-owner-id.vo";
 import { GuestToken } from "../value-objects/guest-token.vo";
 
+
 export interface ICheckoutRepository {
   save(checkout: Checkout): Promise<void>;
   findById(checkoutId: CheckoutId): Promise<Checkout | null>;
@@ -14,10 +15,6 @@ export interface ICheckoutRepository {
   findByGuestToken(guestToken: GuestToken): Promise<Checkout[]>;
   findPendingCheckouts(): Promise<Checkout[]>;
   findExpiredCheckouts(): Promise<Checkout[]>;
-
-  markAsCompleted(checkoutId: CheckoutId, completedAt: Date): Promise<void>;
-  markAsExpired(checkoutId: CheckoutId): Promise<void>;
-  markAsCancelled(checkoutId: CheckoutId): Promise<void>;
 
   cleanupExpiredCheckouts(): Promise<number>;
 }
